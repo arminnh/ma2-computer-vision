cbbc91f796910546892847cb51635d764dc99dd8
Armin <halilovic.armin@hotmail.com>
2018-04-12 19:27:26 +0200

    Added model building for face detection

--- assignments/1-smoothing_filter/smoothing2.py
+++ assignments/1-smoothing_filter/smoothing2.py
@@ -1,6 +1,6 @@
-'''
+"""
 Gaussian smoothing with Python and OpenCV.
-'''
+"""
 
 import cv2
 import math
@@ -8,27 +8,27 @@ import numpy as np
 
 
 def gaussian_smooth2(img, sigma):
-    '''
+    """
     Do gaussian smoothing with sigma.
     Returns the smoothed image.
-    '''
+    """
     # determine the length of the filter
     filter_length = math.ceil(sigma * 5)
     # make the length odd
-    filter_length= 2*(int(filter_length)/2) + 1
+    filter_length = 2 * (int(filter_length) / 2) + 1
 
     # Go ahead!
     # Tip: smoothing=blurring, a filter=a kernel
     return cv2.GaussianBlur(img, (filter_length, filter_length), sigma)
 
 
-#this part of the code is only executed if the file is run stand-alone
+# this part of the code is only executed if the file is run stand-alone
 if __name__ == '__main__':
     # read an image
     img = cv2.imread('image.jpg')
 
     # show the image
-    cv2.imshow('img',img)
+    cv2.imshow('img', img)
 
     # smooth the image
     smoothed_img = gaussian_smooth2(img, 2)
--- assignments/3-facial_recognition/skeleton.py
+++ assignments/3-facial_recognition/skeleton.py
@@ -30,7 +30,7 @@ def detect_and_save_faces(name, roi_size):
     return rectangles
 
 
-def show_rectangles_on_images(image_names, rectangles):
+def save_rectangles_on_images(show, image_names, rectangles):
     for filename in image_names:
         img = cv2.imread(filename)
         subpath, img_name = os.path.split(filename)
@@ -39,31 +39,45 @@ def show_rectangles_on_images(image_names, rectangles):
             cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 255), 2)
 
         cv2.imwrite(os.path.join(subpath, "rectangles", img_name), img)
-        # cv2.imshow('img', img)
-        # cv2.waitKey(0)
+        if show:
+            cv2.imshow("Rectangle on {}".format(img_name), img)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
 
 
 def load_images_as_rows(image_paths, shape):
     N = len(image_paths)
     P = shape[0] * shape[1]
-    X = np.zeros(shape=(N, P))
+    X = np.zeros(shape=(N, P), dtype=np.uint8)
 
     for i, face_path in enumerate(image_paths):
-        print(i, face_path, cv2.imread(face_path))
-        X[i] = cv2.imread(face_path).flatten()
-        # read image in grayscale
-        # X[i] = cv2.imread(face_path, 0).flatten()
+        img = cv2.imread(face_path, 0)
+        img_resized = cv2.resize(img, dsize=shape)
+        X[i] = img_resized.flatten()
 
     return X
 
 
-def do_pca_and_build_model(name, roi_size, images):
+def pca(X, number_of_components):
+    mean = np.mean(X, axis=0).astype(np.uint8)
+    eigenvalues = []
+    _, eigenvectors = cv2.PCACompute(X, mean=None, maxComponents=number_of_components)
+
+    eigenvectors += abs(eigenvectors.min())
+    eigenvectors /= eigenvectors.max()
+    eigenvectors *= 255
+    eigenvectors = eigenvectors.astype(np.uint8)
+
+    return [mean, eigenvalues, eigenvectors]
+
+
+def do_pca_and_build_model(name, shape, images):
     # define where to look for the detected faces
     dir_faces = os.path.join("data", name, "faces")
     faces_paths = [os.path.join(dir_faces, "{}.jpg".format(n)) for n in images]
 
     # put all faces as data vectors in a N x P data matrix X with N the number of faces and the number of pixels
-    X = load_images_as_rows(faces_paths, roi_size)
+    X = load_images_as_rows(faces_paths, shape)
 
     # calculate the eigenvectors of X
     mean, eigenvalues, eigenvectors = pca(X, number_of_components=len(faces_paths))
@@ -71,17 +85,39 @@ def do_pca_and_build_model(name, roi_size, images):
     return [mean, eigenvalues, eigenvectors]
 
 
-def show_model(model):
-    print("TODO Show model", model)
+def save_model_images(show, name, model, shape):
+    mean, eigenvalues, eigenvectors = model
+    dir_model = os.path.join("data", name, "model")
+
+    cv2.imwrite(os.path.join(dir_model, "mean.jpg"), mean.reshape(shape))
+    if show:
+        cv2.imshow("Mean of {} images".format(name), mean.reshape(shape))
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+    eigenvector_imgs = [e.reshape(shape) for e in eigenvectors]
+    cv2.imwrite(os.path.join(dir_model, "eigenvectors.jpg"), np.concatenate(eigenvector_imgs, axis=1))
+    for i, eigenvector_img in enumerate(eigenvector_imgs):
+        cv2.imwrite(os.path.join(dir_model, "eigenvector_{}.jpg".format(i)), eigenvector_img)
+    if show:
+        cv2.imshow("Eigenvectors of {} images".format(name), np.concatenate(eigenvector_imgs, axis=1))
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
 
-def ttest_images(name, roi_size, images, models):
+def project_and_reconstruct(X, model):
+    # TODO
+
+    return [projections, reconstructions]
+
+
+def ttest_images(name, shape, images, models):
     # define where to look for the detected faces
-    dir_faces = "data/{}/faces".format(name)
+    dir_faces = os.path.join("data", name, "faces")
     faces_paths = [os.path.join(dir_faces, "{}.jpg".format(n)) for n in images]
 
     # put all faces as data vectors in a N x P data matrix X with N the number of faces and the number of pixels
-    X = load_images_as_rows(faces_paths, roi_size)
+    X = load_images_as_rows(faces_paths, shape)
 
     # reconstruct the images in X with each of the models provided and also calculate the MSE
     # store the results as a list of [results_model_reconstructed_X, results_model_MSE]
@@ -94,32 +130,23 @@ def ttest_images(name, roi_size, images, models):
     return results
 
 
-def pca(X, number_of_components):
-    # TODO
-
-    return [mean, eigenvalues, eigenvectors]
-
-
-def project_and_reconstruct(X, model):
-    # TODO
-
-    return [projections, reconstructions]
-
-
-def show_reconstructed_images(results):
+def save_reconstructed_images(show, results):
     print("TODO show reconstructed", results)
 
 
 if __name__ == '__main__':
     ROI_SIZE = (50, 50)  # reasonably quick computation time
 
+    SHOW_IMAGES = False
+    SHOW_IMAGES = True  # TODO: comment this line when submitting
+
     # Detect all faces in all the images of arnold and barack and save them in a subdirectory "faces"
-    rectangles_arnold = detect_and_save_faces("arnold", ROI_SIZE)
-    rectangles_barack = detect_and_save_faces("barack", ROI_SIZE)
+    # rectangles_arnold = detect_and_save_faces("arnold", ROI_SIZE)
+    # rectangles_barack = detect_and_save_faces("barack", ROI_SIZE)
 
     # visualize detected ROIs overlaid on the original images and copy paste these figures in a document
-    show_rectangles_on_images(rectangles_arnold.keys(), rectangles_arnold)  # TODO: comment this line when submitting
-    show_rectangles_on_images(rectangles_barack.keys(), rectangles_barack)  # TODO: comment this line when submitting
+    # save_rectangles_on_images(SHOW_IMAGES, rectangles_arnold.keys(), rectangles_arnold)
+    # save_rectangles_on_images(SHOW_IMAGES, rectangles_barack.keys(), rectangles_barack)
 
     # Perform PCA on the previously saved ROIs and build a model for the corresponding person's face
     # making use of a training set. model = [mean, eigenvalues, eigenvectors]
@@ -127,15 +154,15 @@ if __name__ == '__main__':
     model_barack = do_pca_and_build_model("barack", ROI_SIZE, images=[1, 2, 3, 4, 5, 6])
 
     # visualize these "models" in some way (of your choice) and copy paste these figures in a document
-    show_model(model_arnold)  # TODO: comment this line when submitting
-    show_model(model_barack)  # TODO: comment this line when submitting
+    # save_model_images(SHOW_IMAGES, "arnold", model_arnold, ROI_SIZE)
+    # save_model_images(SHOW_IMAGES, "barack", model_barack, ROI_SIZE)
 
     # Test and reconstruct "unseen" images and check which model best describes it (wrt MSE)
     # results are lists of [results_model_reconstructed_X, results_model_MSE]
     # The correct model-person combination should give best reconstructed images and therefore the lowest MSEs
     results_arnold = ttest_images("arnold", ROI_SIZE, images=[7, 8], models=[model_arnold, model_barack])
-    results_barack = ttest_images("barack", ROI_SIZE, images=[7, 8, 9, 10], models=[model_arnold, model_barack])
+    # results_barack = ttest_images("barack", ROI_SIZE, images=[7, 8, 9, 10], models=[model_arnold, model_barack])
 
     # visualize the reconstructed images and copy paste these figures in a document
-    show_reconstructed_images(results_arnold)  # TODO: comment this line when submitting
-    show_reconstructed_images(results_barack)  # TODO: comment this line when submitting
+    save_reconstructed_images(SHOW_IMAGES, results_arnold)
+    # save_reconstructed_images(SHOW_IMAGES, results_barack)
